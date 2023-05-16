42c672b0f0032b40cb2492da683dfc5a78987ed6
FilippoLampa <pippolampa@gmail.com>
2023-05-16 10:54:46 +0200

    [add] soil, tree and apple 3D icon models (w/out textures)

--- src/patmodel/AppleOrchard.java
+++ src/patmodel/AppleOrchard.java
@@ -18,9 +18,13 @@ import repast.simphony.space.grid.SimpleGridAdder;
 
 public class AppleOrchard extends DefaultContext<Object> implements ContextBuilder<Object>{
 
+	private static final double RAINING_NUTRIENTS_TRESHOLD = 0.5;
+	private static final double SUNNY_NUTRIENTS_TRESHOLD = -0.1;
+
 	private Context<Object> context;
 	private Grid<Object> grid;
 	private ContinuousSpace<Object> space;
+	private ContinuousSpace<Object> space2;
 	
 	private boolean isRaining;
 	private boolean isSunny;
@@ -28,23 +32,33 @@ public class AppleOrchard extends DefaultContext<Object> implements ContextBuild
 	
 	private double nutrients;
 	
+	public static final int TREE_VISUALISATION_Y_OFFSET = 16;
+	
 	@Override
 	public Context<Object> build(Context<Object> context) {
 		
-		context.setId("PinkAppleModel");
+		context.setId("PATModel");
 
 		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
 		this.space = spaceFactory.createContinuousSpace (" space ", context, new RandomCartesianAdder < Object >(),
-				new repast.simphony.space.continuous.InfiniteBorders<>(), 100, 100, 100);
+				new repast.simphony.space.continuous.BouncyBorders(), 100, 100, 100);
 	
+		/*ContinuousSpaceFactory spaceFactory2 = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+		this.space2 = spaceFactory2.createContinuousSpace ("soil", context, new RandomCartesianAdder < Object >(),
+				new repast.simphony.space.continuous.BouncyBorders(), 100, 20, 100);
+		
 		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
  		this.grid = gridFactory.createGrid("grid", context, GridBuilderParameters.multiOccupancyND( 
- 				new SimpleGridAdder<Object>(), new InfiniteBorders<>(), 100, 100, 100));
+ 				new SimpleGridAdder<Object>(), new InfiniteBorders<>(), 100, 100, 100));*/
 
  		this.context = context;
  		
  		Parameters p;
  	
+ 		SoilDesign soil = new SoilDesign();
+		context.add(soil);
+		space.moveTo(soil, 50, 0, 50);
+		
  		this.isRaining = false;
  		this.isWindy = false;
  		this.isSunny = false;
@@ -58,19 +72,19 @@ public class AppleOrchard extends DefaultContext<Object> implements ContextBuild
 	private void initAgents() {
 		Tree t = new Tree(space, grid, Tree.BASE_TREE_WIDHT, Tree.BASE_TREE_HEIGHT, Tree.BASE_TREE_AGE, Tree.BASE_APPLE_QUANTITY, Tree.BASE_FOLIAGE_DIAMETER);
 		this.context.add(t);
-		this.space.moveTo(t, 0, 0, 0);
+		this.space.moveTo(t, 0, this.TREE_VISUALISATION_Y_OFFSET, 0);
 	}
 	
 	public void rain(boolean state) {
-		
+		this.isRaining = state;
 	}
 
 	public void wind(boolean state) {
-
+		this.isWindy = state;
 	}
 
 	public void sun(boolean state) {
-
+		this.isSunny = state;
 	}
 
 	@ScheduledMethod(start = 1, interval = 1, priority = 2)
@@ -84,11 +98,17 @@ public class AppleOrchard extends DefaultContext<Object> implements ContextBuild
 
 	private void deltaNutrients() {
 		if(isRaining) {
-			
+			nutrients += RAINING_NUTRIENTS_TRESHOLD;
+		} else if(isSunny) {
+			nutrients += SUNNY_NUTRIENTS_TRESHOLD;
 		}
-			
 	}
 	
+	//TODO rimuovo quando sarà implementato il tuple space
+	
+	public void addNutrients(double nutrients) {
+		this.nutrients += nutrients;
+	}
 	
 		
 }
